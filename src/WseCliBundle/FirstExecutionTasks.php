7079716b9b58dc4dfcfe8568fbae871b525211f0
bslezak <brian@theslezaks.com>
2017-05-18 18:54:05 -0500

    Final cleanup before 1.0.0-alpha4 release. Now usable in global!
    - Cleaned up FirstExecutionTasks and added comments
    - Had to add composer as a requirement since we're using composer source to set up parameters.yml file
    - Updated version to 1.0.0-alpha4

--- src/WseCliBundle/FirstExecutionTasks.php
+++ src/WseCliBundle/FirstExecutionTasks.php
@@ -2,29 +2,44 @@
 namespace WseCliBundle;
 
 use Composer\Console\Application;
-use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Input;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\Output;
 
+/**
+ * FirstExecutionTasks
+ * Performs first time execution configuration tasks
+ * 
+ * @author Brian Slezak <brian@theslezaks.com>
+ *        
+ */
 class FirstExecutionTasks
 {
 
+    /**
+     *
+     * @param string $rootDir
+     *            The root folder of the AppKernel
+     */
     public static function checkFistTimeConfig($rootDir)
     {
+        // Search config path
         $paths = array(
             $rootDir . '/config'
         );
         $locater = new FileLocator($paths);
         try {
+            
+            // Try to located the parameters.yml file. If it doesn't exist catch the Exception and create it
             $locater->locate('parameters.yml');
         } catch (\Exception $e) {
             
-            // parameters.yml was not found so run script to create it
+            // parameters.yml was not found so create an instance of composer to run scripts
             $composer = new Application();
             
+            // Simulate command line argv
             $args = array(
                 0 => "composer",
                 1 => "run-script",
@@ -32,9 +47,10 @@ class FirstExecutionTasks
             );
             $input = new ArgvInput($args);
             
-            $stdout = fopen('php://stdout', 'w');
+            // Get console output for user input
             $output = new ConsoleOutput(Output::VERBOSITY_NORMAL);
             
+            // Run composer which will set up necessary files and prompt user for input
             $composer->run($input, $output);
         }
     }
